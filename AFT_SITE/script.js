a4c2fc04899e7959e0e1e49675d3a9137214d278
I24712I <24712@users.noreply.github.com>
2026-02-12 17:01:11 +0200

    V3

---
+++ AFT_SITE/script.js
@@ -0,0 +1,7 @@
+
+let ar=true;
+function toggleLang(){
+ ar=!ar;
+ document.getElementById('title').innerText= ar?'شركة AFT للتقنية':'AFT Technology';
+ document.getElementById('s1').innerText= ar?'تصميم PCB':'PCB Design';
+}
